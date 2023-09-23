8b3199e3182fa50b3b8d16b2be870c5da8b70443
ekwska <ekwiecinska55@gmail.com>
2023-09-23 18:22:41 +0100

    WIP: Change to image-based input upload on the server

--- airflow_bento_mlflow_mnist_project/bentoml_service.py
+++ airflow_bento_mlflow_mnist_project/bentoml_service.py
@@ -1,15 +1,28 @@
 import bentoml
+from bentoml.io import Image as BMLImage
 from bentoml.io import NumpyNdarray
+from torchvision import transforms
+from typing import Any
+
+from numpy.typing import NDArray
 import numpy as np
 
 
 def create_service(service_name, runner):
     svc = bentoml.Service(service_name, runners=[runner])
 
-    @svc.api(input=NumpyNdarray(), output=NumpyNdarray())
-    def classify(input_series: np.ndarray) -> np.ndarray:
-        result = runner.predict.run(input_series)
-        return result
+    @svc.api(input=BMLImage(), output=NumpyNdarray(dtype="float32"))
+    async def predict_image(f: BMLImage) -> NDArray[Any]:
+        # TODO: The image is empty here so predictions can't be made, investigate.
+        transform = transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+        )
+        arr = np.array(transform(f))
+        assert arr.shape == (1, 28, 28)
+
+        # We are using greyscale image and our PyTorch model expect one
+        # extra channel dimension
+        return await runner.async_run(arr)
 
     return svc
 
